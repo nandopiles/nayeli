cc6d24305cd72086adbcfe324d7bce7cb989ebf1
Nando Piles <ferran.piles@onestic.com>
2024-03-21 13:57:08 +0100

    db credentials configured

---
+++ api/app/config.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql://uz0fhehaw2vdwhmo:uz0fhehaw2vdwhmo@bdwdn5gpcyklsebxusgc-mysql.services.clever-cloud.com/bdwdn5gpcyklsebxusgc'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
