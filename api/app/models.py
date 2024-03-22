98ddbdde13802a7a9e6e8b9f47e3e56a8812b2d5
Nando Piles <ferran.piles@onestic.com>
2024-03-22 14:52:11 +0100

    models

--- api/app/models.py
+++ api/app/models.py
@@ -1,12 +1,11 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from app import db
 
 
+# Define los modelos
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
 
-    def __repr__(self):
-        return "<User %r>" % self.username
+    def serialize(self):
+        return {"id": self.id, "username": self.username, "password": self.password}
