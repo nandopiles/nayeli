a557077e0462d55af23089b7070fffb5a772dc64
Nando <nandopiless@gmail.com>
2024-03-24 17:34:49 +0100

    request for returning an specific user

--- api/app/routes.py
+++ api/app/routes.py
@@ -3,8 +3,18 @@ from app import app
 from app.models import User
 
 
-# Define las rutas
-@app.route("/people")
+@app.route("/users", methods=["GET"])
 def get_users():
+    """Gets all the users from the db"""
     users = User.query.all()
     return jsonify([user.serialize() for user in users])
+
+
+@app.route("/user/<int:user_id>", methods=["GET"])
+def get_user(user_id):
+    """Gets a specific user from the db depending on its id"""
+    user = User.query.get(user_id)
+    if user:
+        return jsonify(user.serialize())
+    else:
+        return jsonify({"error": "User not found"}), 404
