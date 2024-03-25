9f8a6e078ec732a5cba5f9a023be7eed4510cbff
Nando Piles <ferran.piles@onestic.com>
2024-03-25 09:24:07 +0100

    __init__ loading config credentials

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,11 +1,22 @@
+from pathlib import Path
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
-# Iniciación aplicación Flask y conf de SQLAlchemy
+# Initiation of Flask app and SQLAlchemy conf
 app = Flask(__name__)
-app.config.from_object(Config)
+
+# Gets the actual directory of __init__.py file
+current_directory = Path(__file__).resolve().parent
+
+# Gets the absolute path of config.json using pathlib
+config_file_path = current_directory.parent / "config.json"
+
+Config.load_from_file(config_file_path)
+app.config["SQLALCHEMY_DATABASE_URI"] = Config.SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = Config.SQLALCHEMY_TRACK_MODIFICATIONS
 
 db = SQLAlchemy(app)
+
 
 from app import routes, models
