c137122cd47a4cea4dfbe71706c72f16edc77e38
Nando Piles <ferran.piles@onestic.com>
2024-04-12 11:03:49 +0200

    user-api-service

--- nayeli-app/src/app/services/user-api-service.service.spec.ts
+++ nayeli-app/src/app/services/user-api-service.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { UserApiService } from './user-api-service.service';
 
-describe('ApiServiceService', () => {
+describe('UserApiServiceService', () => {
   let service: UserApiService;
 
   beforeEach(() => {
