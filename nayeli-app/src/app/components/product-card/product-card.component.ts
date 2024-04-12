c443973d52b7fe89073e84e12a16a7927a509e4d
Nando Piles <ferran.piles@onestic.com>
2024-04-12 12:35:55 +0200

    product card with api access

--- nayeli-app/src/app/components/product-card/product-card.component.ts
+++ nayeli-app/src/app/components/product-card/product-card.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../interfaces/nayeli.interface';
+import { ProductApiService } from '../../services/product-api-service.service';
 
 @Component({
   selector: 'app-product-card',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
-export class ProductCardComponent {
+export class ProductCardComponent implements OnInit {
+  public products: Product[] = []
 
+  public constructor(public service: ProductApiService) { }
+
+  public getProducts(): void {
+    this.service.getAllProducts().subscribe((response) => {
+      this.products = response
+      console.log(response);
+
+    });
+  }
+
+  ngOnInit(): void {
+    this.getProducts()
+  }
 }
