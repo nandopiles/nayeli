f3cb8b5ffe3daeb8415bac4f0ce5892cc6bd52ee
Nando Piles <ferran.piles@onestic.com>
2024-04-15 13:56:45 +0200

    route products

--- nayeli-app/src/app/app.routes.ts
+++ nayeli-app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ProductGridComponent } from './components/product-grid/product-grid.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'products', pathMatch: 'full' },
+    { path: 'products', component: ProductGridComponent }
+];
