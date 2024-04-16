feb329df517b146d72b218256735d4aba4190f24
Nando Piles <ferran.piles@onestic.com>
2024-04-16 10:21:56 +0200

    form for product size

--- nayeli-app/src/app/components/product-detail/product-detail.component.ts
+++ nayeli-app/src/app/components/product-detail/product-detail.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
-
+  size = new FormControl('XS')
 }
