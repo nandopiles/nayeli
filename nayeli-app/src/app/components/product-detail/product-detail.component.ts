6cf91e1583b040550ab2f86076ad471027abf378
Nando Piles <ferran.piles@onestic.com>
2024-04-18 13:04:35 +0200

    more eficient code to choose the size

--- nayeli-app/src/app/components/product-detail/product-detail.component.ts
+++ nayeli-app/src/app/components/product-detail/product-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { Product } from '../../interfaces/nayeli.interface';
+import { Product, SizeOption } from '../../interfaces/nayeli.interface';
 import { ProductApiService } from '../../services/product-api-service.service';
 import { SpinnerComponent } from '../spinner/spinner.component';
 
@@ -22,12 +22,46 @@ export class ProductDetailComponent implements OnInit {
     price: 0
   };
   isLoading: boolean = true;
-  size = new FormControl('None');
+  sizeSelected = new FormControl('');
 
   constructor(
     private route: ActivatedRoute,
     private service: ProductApiService
   ) { }
+
+  /**
+   * Gets the size values of the product selected.
+   * @returns {void}
+   */
+  getSizeOptions(): SizeOption[] {
+    if (this.isFootwear()) {
+      return [
+        { label: '38', value: '38', disabled: false },
+        { label: '39', value: '39', disabled: false },
+        { label: '40', value: '40', disabled: false },
+        { label: '41', value: '41', disabled: true },
+        { label: '42', value: '42', disabled: false },
+        { label: '43', value: '43', disabled: false },
+        { label: '44', value: '44', disabled: true },
+        { label: '45', value: '45', disabled: false },
+      ];
+    } else if (this.isClothing()) {
+      return [
+        { label: 'XXS', value: 'XXS', disabled: true },
+        { label: 'XS', value: 'XS', disabled: false },
+        { label: 'S', value: 'S', disabled: false },
+        { label: 'M', value: 'M', disabled: true },
+        { label: 'L', value: 'L', disabled: false },
+        { label: 'XL', value: 'XL', disabled: false },
+      ];
+    } else if (this.isAccessories()) {
+      return [
+        { label: 'Talla Única', value: 'Talla Única', disabled: false },
+      ];
+    } else {
+      return [];
+    }
+  }
 
   /**
    * Checks if the category of the product is 'Footwear'
