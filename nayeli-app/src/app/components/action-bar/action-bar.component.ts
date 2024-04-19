63eab36d0f3d376f5c0c6e321504ec0600f4b78b
Nando Piles <ferran.piles@onestic.com>
2024-04-19 10:09:58 +0200

    reactive form actionbar

--- nayeli-app/src/app/components/action-bar/action-bar.component.ts
+++ nayeli-app/src/app/components/action-bar/action-bar.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from '../../interfaces/nayeli.interface';
 import { CategoryApiService } from '../../services/category-api.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-action-bar',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './action-bar.component.html',
   styleUrl: './action-bar.component.css'
 })
 export class ActionBarComponent implements OnInit {
   categories: Category[] = [];
+  filterForm = new FormGroup({
+    category: new FormControl('all'),
+    name: new FormControl('')
+  });
 
   public constructor(public service: CategoryApiService) { }
 
@@ -23,6 +28,12 @@ export class ActionBarComponent implements OnInit {
       this.categories = categories;
       console.log(this.categories);
     })
+  }
+
+  onSubmit() {
+    // Handle form submission logic here
+    // Access filtered data using this.filterForm.value
+    console.log('Filtered by:', this.filterForm.value);
   }
 
   ngOnInit(): void {
