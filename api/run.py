1cf5c68bec099604a66233489ec31fec02f460e6
nando <you@example.com>
2024-04-21 17:07:25 +0000

    test

--- api/run.py
+++ api/run.py
@@ -1,4 +1,11 @@
-from app import app
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
