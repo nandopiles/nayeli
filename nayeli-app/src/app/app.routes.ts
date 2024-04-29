41f63c77b6ce5b36daafda5ad0a5a840549c1fec
Nando Piles <ferran.piles@onestic.com>
2024-04-29 13:03:35 +0200

    layouts structure created

--- nayeli-app/src/app/app.routes.ts
+++ nayeli-app/src/app/app.routes.ts
@@ -4,12 +4,26 @@ import { ProductsComponent } from './views/products/products.component';
 import { ProductDetailComponent } from './views/product-detail/product-detail.component';
 import { ContactComponent } from './views/contact/contact.component';
 import { LoginComponent } from './views/login/login.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { LoginLayoutComponent } from './layouts/login-layout/login-layout.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    { path: 'products', component: ProductsComponent },
-    { path: 'product/:id', component: ProductDetailComponent },
-    { path: 'contact', component: ContactComponent },
-    { path: 'login', component: LoginComponent }
+    {
+        path: '',
+        component: MainLayoutComponent,
+        children: [
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: 'products', component: ProductsComponent },
+            { path: 'product/:id', component: ProductDetailComponent },
+            { path: 'contact', component: ContactComponent }
+        ]
+    },
+    {
+        path: '',
+        component: LoginLayoutComponent,
+        children: [
+            { path: 'login', component: LoginComponent }
+        ]
+    }
 ];
