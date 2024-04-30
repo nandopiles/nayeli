2776343f3cee75b3403119c328a3ecf8f292639f
Nando Piles <ferran.piles@onestic.com>
2024-04-30 11:40:51 +0200

    view created

---
+++ nayeli-app/src/app/views/user-zone/user-zone.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-zone',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-zone.component.html',
+  styleUrl: './user-zone.component.css'
+})
+export class UserZoneComponent {
+
+}
